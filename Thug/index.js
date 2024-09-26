41534c3035087b92e6a6c0786acd13f46094503a
shratt <123132403+shratt@users.noreply.github.com>
2024-09-26 16:11:42 -0400

    allow for userscript

--- Thug/index.js
+++ Thug/index.js
@@ -1,10 +1,3 @@
-import thugCore from "./API";
 import initializeMenu from "./UI/menu"
-
-if (document.readyState == "complete") { 
-    thugCore.init();
-} else {
-    window.addEventListener("load", thugCore.init);
-}
 
 initializeMenu();
