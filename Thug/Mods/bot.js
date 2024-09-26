fcf8a4fd90b4ba7066d4e1b99a4baf075a6d5595
shratt <123132403+shratt@users.noreply.github.com>
2024-09-26 16:47:27 -0400

    better bot

--- Thug/Mods/bot.js
+++ Thug/Mods/bot.js
@@ -1,18 +1,17 @@
+import actions from "../API/actions";
+
 function botMeeting (amount) {
     localStorage.clear();
     window.meetingURL = window.meetingURL || document.querySelector("#webclient").src;
     document.getElementById("root").innerHTML = "";
     
-    let offset = 0;
-    
     for (let i = 0; i < amount; i++) {
         let menu = document.createElement('iframe');
-        menu.width = "250"
+        menu.width = "100%"
+        menu.height = "100%"
         menu.src = meetingURL;
         document.getElementById("root").appendChild(menu);
-    
-        offset += menu.width;
-    
+
         let scope = menu.contentWindow;  
         (scope.webpackChunkwebclient = scope.webpackChunkwebclient || []).push([[Symbol()], {}, function (require) {
             require(22665).$c.webClient_meetingUqiueId = Math.random().toString(36).substring(2,7); // bypass zoom checking UID
