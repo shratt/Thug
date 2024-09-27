1e44d92633f3fe590f3c96e448f4082ac0d771e6
shratt <123132403+shratt@users.noreply.github.com>
2024-09-27 14:03:26 -0400

    less bloat + better bots

--- Thug/Mods/bot.js
+++ Thug/Mods/bot.js
@@ -1,14 +1,18 @@
 import actions from "../API/actions";
 
+let offset = 0;
+
 function botMeeting (amount) {
     localStorage.clear();
     window.meetingURL = window.meetingURL || document.querySelector("#webclient").src;
 
-    let botWindow = window.open("about:blank", "Bot Panel", "width=500,height=400,left=100,top=100")
+    let botWindow = window.open("about:blank", "Bot Panel - " + offset, "width=500,height=400,left=100,top=100")
+
+    offset++;
 
     botWindow.document.title = "Bot Panel";
     botWindow.document.body.style.backgroundColor = "black";
-    
+
     let container = document.createElement("div");
     container.style.display = "column wrap";
     botWindow.document.body.appendChild(container);
--- Thug/config.js
+++ Thug/config.js
@@ -1,4 +1,20 @@
-window.namesList = ["john smith", "grandmas ipad", "john", "ben", "bob"];
+window.namesList = [
+    "James", "Mary", "John", "Patricia", "Robert", "Jennifer", "Michael", "Linda", "William", "Elizabeth",
+    "David", "Barbara", "Richard", "Susan", "Joseph", "Jessica", "Thomas", "Sarah", "Charles", "Karen",
+    "Christopher", "Nancy", "Daniel", "Lisa", "Matthew", "Margaret", "Anthony", "Betty", "Donald", "Sandra",
+    "Mark", "Ashley", "Paul", "Dorothy", "Steven", "Kimberly", "Andrew", "Emily", "Joshua", "Donna",
+    "Kevin", "Michelle", "Brian", "Carol", "George", "Amanda", "Edward", "Melissa", "Ronald", "Deborah",
+    "Samuel", "Helen", "Adam", "Natalie", "Diana", "Peter", "Victoria", "Henry", "Julia",
+    "Ryan", "Alice", "Nathan", "Sophia", "Isaac", "Grace", "Zachary", "Chloe", "Jesse", "Megan",
+    "Gabriel", "Ava", "Ethan", "Lily", "Lucas", "Scarlett", "Noah", "Emma", "Logan", "Hannah",
+    "Aiden", "Samantha", "Isaiah", "Addison", "Caleb", "Nora", "Charles", "Katherine", "Luke", "Zoe",
+    "Leo", "Aria", "Oliver", "Bella", "Jackson", "Aurora", "Jameson", "Madison", "Eli", "Piper",
+    "Mason", "Sofia", "Carter", "Ellie", "Dylan", "Stella", "Sawyer", "Victoria", "Theodore", "Hazel",
+    "Jordan", "Riley", "Wyatt", "Layla", "Hudson", "Maya", "Asher", "Sadie", "Anthony", "Aubrey",
+    "Gavin", "Claire", "Adrian", "Luna", "Chase", "Eliana", "Jaxon", "Kinsley", "Colton", "Maddison",
+    "Xander", "Camila", "Kai", "Serenity", "Liam", "Julia", "Quinn", "Vivian", "Ryder", "Sienna"
+];
+
 window.reactionList = [
     "💀", "☠", "🤣", "😂", "😄", "😅", "😊", "😇", "😉", "😍", 
     "😘", "😜", "🤪", "🤩", "😎", "😏", "😡", "😱", "😳", "😢", 
