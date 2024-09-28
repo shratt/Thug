ca44224fa008bd55596adb43a33114a5e09108af
darian <116446349+justDarian@users.noreply.github.com>
2024-09-27 21:38:13 -0400

    bru

--- src/Mods/autoRejoin.js
+++ src/Mods/autoRejoin.js
@@ -2,7 +2,7 @@ import core from "../API";
 
 function autoRejoin () {
     autoRejoin.autoRejoinInterval = setInterval(function () {
-        if (core.frame.document.getElementsByClassName("zm-btn zm-btn-legacy zm-btn--primary")?.[0]?.innerText == "Exit") {
+        if (core.frame.document.getElementsByClassName("zm-btn zm-btn-legacy zm-btn--primary")?.[0]?.innerText == "Exit" || document.getElementsByClassName("zm-btn zm-btn-legacy zm-btn--primary zm-btn__outline--blue")?.[0]?.innerText === "Leave") {
             localStorage.clear();
             sessionStorage.clear();
             core.frame.location.reload();
